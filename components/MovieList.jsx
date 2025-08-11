2c73fecea7cba54492590f5517135f8da0fb0374
HabibaHamdyyy <hamdyhabiba952@gmail.com>
2025-08-11 17:45:24 +0300

    Add files via upload

---
+++ components/MovieList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import MovieCard from "./MovieCard";
+
+const MovieList = ({ movies }) => {
+  if (!movies.length) return <p>No movies found.</p>;
+
+  return (
+    <div className="movie-list">
+      {movies.map((movie) => (
+        <MovieCard key={movie.imdbID} movie={movie} />
+      ))}
+    </div>
+  );
+};
+
+export default MovieList;
